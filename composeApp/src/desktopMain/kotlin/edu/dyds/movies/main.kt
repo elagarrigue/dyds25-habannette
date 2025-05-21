c8da3c5a20ca3b50c312010bef19dee575ae138a
LaureanoRamirez <lauramirez2611@gmail.com>
2025-05-21 15:24:11 -0300

    Rama creada

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/main.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/main.kt
@@ -2,7 +2,7 @@ package edu.dyds.movies
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
+// Rama creada, comentario de prueba
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,

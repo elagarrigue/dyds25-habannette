8785957f91b95fb065e1cd335f48b5dafa62cc1a
elagarrigue <elagarriguelazarte@doximity.com>
2025-06-14 13:13:57 -0300

    Fix format

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/MoviesExternalSource.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/MoviesExternalSource.kt
@@ -3,6 +3,6 @@ package edu.dyds.movies.data.external
 import edu.dyds.movies.domain.entity.Movie
 
 interface MoviesExternalSource {
-        suspend fun getMovies(): List<Movie>
-        suspend fun getMovieDetails(id: Int): Movie?
+    suspend fun getMovies(): List<Movie>
+    suspend fun getMovieDetails(id: Int): Movie?
 }

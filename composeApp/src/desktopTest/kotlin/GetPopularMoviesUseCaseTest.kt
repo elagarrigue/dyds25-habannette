080f474032b15d7b5deb60f477355bfe6178b67e
Federico Rojo <federojo10@gmail.com>
2025-06-19 13:33:14 -0300

    Corrección de UseCase Tests

--- composeApp/src/desktopTest/kotlin/GetPopularMoviesUseCaseTest.kt
+++ composeApp/src/desktopTest/kotlin/GetPopularMoviesUseCaseTest.kt
@@ -2,7 +2,7 @@ import edu.dyds.movies.domain.entity.Movie
 import edu.dyds.movies.domain.repository.MoviesRepository
 import edu.dyds.movies.domain.usecase.GetPopularMoviesUseCase
 import edu.dyds.movies.domain.usecase.GetPopularMoviesUseCaseImpl
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,7 +13,7 @@ class GetPopularMoviesUseCaseTest{
 
 
     @Test
-    fun `getPopularMovies obtiene Movies y las ordena de forma descendente`() = runBlocking {
+    fun `getPopularMovies obtiene Movies y las ordena de forma descendente`() = runTest {
         //Arrange
         val m1 = Movie(id=1, title="A", "algo", "11/6/2025",
             "algo", "algo", "PeliculaA","Ingles", 2.5, 8.0     )
@@ -22,32 +22,32 @@ class GetPopularMoviesUseCaseTest{
         val m3 = Movie(id=3, title="C", "algo", "11/6/2025",
             "algo", "algo", "PeliculaC","Ingles", 2.5, 7.0     )
 
-        val repository = object : MoviesRepository {
+        repository = object : MoviesRepository {
             override suspend fun getPopularMovies(): List<Movie> =
                 listOf(m1, m2, m3)
 
             override suspend fun getMovieDetails(id: Int): Movie? = null
         }
-        val useCase = GetPopularMoviesUseCaseImpl(repository)
+        useCase = GetPopularMoviesUseCaseImpl(repository)
 
         //Act
         val resultado = useCase.getPopularMovies()
 
         //Assert
         assertEquals(listOf(1,3,2), resultado.map{it.movie.id} )
-        assertEquals(resultado[0].isGoodMovie, true)
-        assertEquals(resultado[1].isGoodMovie, false)
-        assertEquals(resultado[2].isGoodMovie, true)
+        assertEquals(true, resultado[0].isGoodMovie)
+        assertEquals(true, resultado[1].isGoodMovie)
+        assertEquals(false, resultado[2].isGoodMovie)
 
     }
 
     @Test
-    fun `getPopularMovies con lista vacia retorna vacio`() = runBlocking {
-        val repository = object : MoviesRepository {
+    fun `getPopularMovies con lista vacia retorna vacio`() = runTest {
+        repository = object : MoviesRepository {
             override suspend fun getPopularMovies(): List<Movie> = emptyList()
             override suspend fun getMovieDetails(id: Int): Movie? = null
         }
-        val useCase = GetPopularMoviesUseCaseImpl(repository)
+        useCase = GetPopularMoviesUseCaseImpl(repository)
 
         val resultado = useCase.getPopularMovies()
         assertEquals(resultado.isEmpty(), true)
